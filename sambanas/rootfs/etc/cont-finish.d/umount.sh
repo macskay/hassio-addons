32afcded1501d52f1326f2fdea480e03a3c1a001
Maximlian Klingmann <max@macskay.com>
2021-09-18 17:06:43 +0200

    by-uuid

--- sambanas/rootfs/etc/cont-finish.d/umount.sh
+++ sambanas/rootfs/etc/cont-finish.d/umount.sh
@@ -12,14 +12,14 @@ if [[ -f /tmp/local_mount ]]; then
     bashio::log.info "Unmount drivers."
     while read -r line; do 
         bashio::log.info "Unmount ${line}"
-        umount /dev/disk/by-label/$line
+        umount /dev/disk/by-uuid/$line
    done < /tmp/local_mount
 fi
 if [[ -f /tmp/remote_mount ]]; then  
     bashio::log.info "Unmount Host drivers."
     while read -r line; do 
         bashio::log.info "Unmount Host ${line}"
-        ssh root@${ipaddress%/*} -p 22222 -o "StrictHostKeyChecking no" "umount /dev/disk/by-label/$line" || true
+        ssh root@${ipaddress%/*} -p 22222 -o "StrictHostKeyChecking no" "umount /dev/disk/by-uuid/$line" || true
     done < /tmp/remote_mount
 fi
 bashio::log.info "Done."
--- sambanas/rootfs/etc/cont-init.d/automount.sh
+++ sambanas/rootfs/etc/cont-init.d/automount.sh
@@ -12,10 +12,10 @@ declare remote_mount
 # mount a disk from parameters
 function mount_disk() {
      if [ "$remote_mount" = true ] ; then
-       ssh root@${ipaddress%/*} -p 22222 -o "StrictHostKeyChecking no" "mount -t auto /dev/disk/by-label/$disk /mnt/data/supervisor/media/$disk -o nosuid,relatime,noexec" \
+       ssh root@${ipaddress%/*} -p 22222 -o "StrictHostKeyChecking no" "mount -t auto /dev/disk/by-uuid/$disk /mnt/data/supervisor/media/$disk -o nosuid,relatime,noexec" \
           && echo $disk >> /tmp/remote_mount
      fi || bashio::log.warning "Host Mount Fail!"
-     mount -t auto /dev/disk/by-label/$disk /media/$disk -o nosuid,relatime,noexec \
+     mount -t auto /dev/disk/by-uuid/$disk /media/$disk -o nosuid,relatime,noexec \
        && echo $disk >> /tmp/local_mount
 }
 
